8d89a0b1cd90e7e0e40d02dc4a64825c518955aa
FernandoDiasH <fernandohiroshi10@hotmail.com>
2022-10-03 11:57:46 -0300

    Atualizando o projeto para laravel

--- database/conn.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include_once 'confg.inc.php';
-
-function conectar ()
-{
-	return mysqli_connect(HOST, USERNAME, PASSWORD, DBNAME);
-}
-
- ?>
--- menu.php
+++
@@ -1,4 +0,0 @@
-<ul>
-	<li><a href="sistema/lancamento/lancamento.php">Lançamento de contas</a></li>
-	<li><a href="sistema/indicador/index.php">Demosntrativo</a></li>
-</ul>
